fa58543345c783eeba840d3e45b957e93c1dca16
jimmy.shen <1247636982@qq.com>
2021-01-19 19:53:05 +0800

    初始化代码

---
+++ src/main/java/com/geek/geekweb/GeekwebApplication.java
@@ -0,0 +1,13 @@
+package com.geek.geekweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekwebApplication.class, args);
+	}
+
+}
